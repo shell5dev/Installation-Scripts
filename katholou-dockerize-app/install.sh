dc2e5e7f88f542f94226691d29f107cc657b2929
Ivan <ivans@vaskir.co>
2019-04-02 15:38:12 +0200

    Fix for Mint

--- katholou-dockerize-app/install.sh
+++ katholou-dockerize-app/install.sh
@@ -57,7 +57,7 @@ function checkDeps(){
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository \
             "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-            $(lsb_release -cs) \
+            xenial \
             stable"
         sudo apt update
         sudo apt-get install docker-ce docker-ce-cli containerd.io -y
